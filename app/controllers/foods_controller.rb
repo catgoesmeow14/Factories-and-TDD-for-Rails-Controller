f9c858addaa016e811fbda6361291b365117ebd8
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-15 12:44:25 +0000

    Change controller and route

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,42 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = params[:letter] ? Food.by_letter(params[:letter]) : Food.all
+  end
+
+  def show
+    @food = Food.find_by(id: params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def edit
+    @food = Food.find_by(id: params[:id])
+  end
+
+  def create
+    @food = Food.new(params.require(:food).permit(:name, :description, :price))
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @food = Food.find_by(id: params[:id])
+    @food.update(params.require(:food).permit(:name))
+    if @food.save
+      redirect_to food_path(@food)
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def destroy
+    @food = Food.find_by(id: params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
-Rails.application.routes.draw do
-  get 'food/index'
-  get 'food/create'
-  get 'food/new'
-  get 'food/destroy'
-  get 'food/show'
-  get 'food/update'
+	Rails.application.routes.draw do
+  # get 'foods/index'
+  # get 'foods/show'
+  # get 'foods/new'
+  # get 'foods/edit'
+  # get 'foods/create'
+  # get 'foods/update'
+  # get 'foods/destroy'
+  resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
