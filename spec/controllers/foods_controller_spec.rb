ed2dc08e1aed0be8e16987c7a62e3f38ba88e89d
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-15 11:42:55 +0000

    Testing DELETE Requests

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -144,10 +144,22 @@ describe FoodsController do
     end
   end
 
-  # destroy method
+  # destroy method and Testing DELETE Requests
   describe 'DELETE #destroy' do
-    it "deletes the food from the database"
-    it "redirects to foods#index"
+    before :each do
+      @food = create(:food)
+    end
+
+    it "deletes the food from the database" do
+      expect{
+        delete :destroy, params: { id: @food }
+      }.to change(Food, :count).by(-1)
+    end
+
+    it "redirects to foods#index" do
+      delete :destroy, params: { id: @food }
+      expect(response).to redirect_to foods_url
+    end
   end
   
 end
