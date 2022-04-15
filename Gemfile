4a4354507935b0796f91f9f97e463b8d4178f289
catgoesmeow14 <exoexo1990@gmail.com>
2022-04-15 10:22:16 +0000

    adding faker to factories, controller, and controller spec

--- Gemfile
+++ Gemfile
@@ -54,7 +54,9 @@ group :development, :test do
   gem "rspec-rails"
   gem "factory_bot_rails"
   gem "faker"
+  gem 'rails-controller-testing'
 end
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'food/index'
+  get 'food/create'
+  get 'food/new'
+  get 'food/destroy'
+  get 'food/show'
+  get 'food/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
